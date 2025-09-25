7cb9deaa31bc92c6ef8f06df1bcd14a277d2c27a
albertasv <awiley@mail.ru>
2025-09-25 20:04:54 +0300

    Update CrptApi.java

--- src/main/java/CrptApi.java
+++ src/main/java/CrptApi.java
@@ -1,210 +1,123 @@
+import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 
 public class CrptApi {
+    private static final int INITIAL_DELAY = 0;
+    private static final Logger logger = Logger.getLogger(CrptApi.class.getName());
 
-    private final String CLIENT_TOKEN = "clientToken";
-    private final String USER_NAME = "userName";
-    private final String URL = "http://<server-name>[:server-port]" +
-            "/api/v3/{extension}/ rollout?omsId={omsId}";
+    private final Semaphore semaphore;
+    private final String url;
+    private final ScheduledExecutorService scheduledExecutorService;
 
-    private int requestLimit;
-    private final TimeUnit timeUnit;
-    private static int counter;
 
-    // ********************************
-    public CrptApi(TimeUnit timeUnit, int requestLimit) {
-        this.timeUnit = timeUnit;
-        if (requestLimit >= 0) {
-            this.requestLimit = requestLimit;
-            counter = requestLimit;
-        } else {
-            throw new IllegalArgumentException("Передано отрицательное число");
-        }
+    public CrptApi(TimeUnit timeUnit, int requestLimit, int intervalTime, String url) {
+        this.semaphore = new Semaphore(requestLimit);
+        this.url = url;
+        this.scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+        scheduledExecutorService.scheduleAtFixedRate(() -> semaphore.release(0),
+                INITIAL_DELAY, intervalTime, timeUnit);
+        logger.info("create semaphore and scheduledExecutorService");
     }
 
-    public void runRequest(Document document, String signature) {
-        String docJson = getDocJson(document, signature).toString();
-        httpRequest(docJson);
-    }
-
-    
-    private JSONObject getDocJson(Document document, String signature) {
-        JSONObject doc = new JSONObject();
-        if (isNull(document.getDescription())) {
-            JSONObject inn = new JSONObject();
-            inn.put("participantInn", document.getParticipantInn());
-            doc.put("description", inn);
-        }
-        doc.put("doc_id", document.getDocId());
-        doc.put("doc_status", document.getDocStatus());
-        doc.put("doc_type", document.getDocType());
-        if (isNull(document.getImportRequest())) {
-            doc.put("importRequest", document.getImportRequest());
-        }
-        doc.put("owner_inn", document.getOwnerInn());
-        doc.put("participant_inn", document.getParticipantInn());
-        doc.put("producer_inn", document.getProducerInn());
-        doc.put("production_date", document.getProducerInn());
-        doc.put("production_type", document.getProductionType());
-        Document.Products product = document.getProducts();
-        if (product != null) {
-            JSONArray productsList = new JSONArray();
-            JSONObject products = new JSONObject();
-            if (product.getCertificateDocument() != null) {
-                products.put("certificate_document", product.getCertificateDocument());
-            } else if (isNull(product.getCertificateDocumentDate())) {
-                products.put("certificate_document_date", product.getCertificateDocumentDate());
-            } else if (isNull(product.getCertificateDocumentNumber())) {
-                products.put("certificate_document_number", product.getCertificateDocumentNumber());
-            }
-            products.put("owner_inn", document.getOwnerInn());
-            products.put("producer_inn", document.getProducerInn());
-            products.put("production_date", document.getProductionDate());
-            if (!document.getProductionDate().equals(product.getProductionDate())) {
-                products.put("production_date", product.getProductionDate());
-            }
-            products.put("tnved_code", product.tnvedCode);
-            if (isNull(product.getUitCode())) {
-                products.put("uit_code", product.getUitCode());
-            } else if (isNull(product.getUituCode())) {
-                products.put("uitu_code", product.getUituCode());
-            } else {
-                throw new IllegalArgumentException("Одно из полей uit_code/uitu_code " +
-                        "является обязательным");
-            }
-            productsList.add(products);
-            doc.put("products", productsList);
-        }
-        doc.put("reg_date", document.getRegDate());
-        doc.put("reg_number", document.getRegNumber());
-        return doc;
-    }
-
-    private void httpRequest(String json) {
-        if (requestLimit != 0) {
-            synchronized (this) {
-                counter--;
-            }
-        }
+    public void createDocument(Document document, String signature) {
         try {
-            if (counter < 0) {
-                Thread.sleep(getTime());
-                counter = requestLimit;
-            }
-            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost post = new HttpPost(URL);
+            semaphore.acquire();
+            logger.info("semaphore access has been");
 
-            StringEntity entity = new StringEntity(json);
-            post.addHeader("content-type", "application/json");
-            post.addHeader("clientToken", CLIENT_TOKEN);
-            post.addHeader("userName", USER_NAME);
-            post.setEntity(entity);
-            httpClient.execute(post);
-            httpClient.close();
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            HttpResponse<String> response = httpRequest(document, signature);
+            logger.info("document has been sent and response received from server, status: " + response.statusCode());
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            logger.severe("interruption during document sending");
+            throw new RuntimeException("interruption during document sending " + e.getMessage());
+        } finally {
+            semaphore.release();
+            logger.info("semaphore released ");
         }
     }
 
-    private boolean isNull(String check) {
-        return check != null;
+    private HttpResponse<String> httpRequest(Document document, String signature) throws InterruptedException {
+        HttpClient httpClient = createHttpClient();
+        logger.info("create httpClient");
+
+        HttpRequest request = createHttpRequest(document, signature);
+        logger.info("create HttpRequest");
+
+        try {
+            return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            logger.severe("error sending document");
+            throw new RuntimeException("error sending document " + e.getMessage());
+        }
     }
 
-    public enum TimeUnit {
-        SECOND, MINUTE, HOUR
+    private HttpClient createHttpClient() {
+        return HttpClient.newHttpClient();
     }
 
-    private long getTime() {
-        return switch (timeUnit) {
-            case SECOND -> 1000;
-            case MINUTE -> 1000 * 60;
-            case HOUR -> 1000 * 60 * 60;
-        };
+    private HttpRequest createHttpRequest(Document document, String signature) {
+        String json = convertToJson(document);
+        return HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .header("signature", signature)
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .build();
     }
 
-    /**
-     * Class Document     
-     */
+    private String convertToJson(Document document) {
+        Gson gson = new Gson();
+        return gson.toJson(document);
+    }
+
+    @Getter
+    @Setter
     public static class Document {
-        @Getter
-        @Setter
-        private String description;
-        @Getter
-        private final String participantInn;
-        @Getter
-        private final String docId;
-        @Getter
-        private final String docStatus;
-        @Getter
-        private final String docType;
-        @Getter
-        @Setter
-        private String importRequest;
-        @Getter
-        private final String ownerInn;
-        @Getter
-        private final String producerInn;
-        @Getter
-        private final String productionDate;
-        @Getter
-        private final String productionType;
-        @Getter
-        private final String regDate;
-        @Getter
-        private final String regNumber;
-        @Getter
-        @Setter
-        private Products products;
 
-        public Document(String participantInn, String docId, String docStatus,
-                        String docType, String ownerInn, String producerInn,
-                        String productionDate, String productionType,
-                        String regDate, String regNumber) {
-            this.participantInn = participantInn;
-            this.docId = docId;
-            this.docStatus = docStatus;
-            this.docType = docType;
-            this.ownerInn = ownerInn;
-            this.producerInn = producerInn;
-            this.productionDate = productionDate;
-            this.productionType = productionType;
-            this.regDate = regDate;
-            this.regNumber = regNumber;
-        }
+        private Description description;
+        private String docId;
+        private String docStatus;
+        private String docType;
+        private Boolean importRequest;
+        private String ownerInn;
+        private String producerInn;
+        private String productionDate;
+        private String productionType;
+        private List<Product> products;
+        private String regDate;
+        private String regNumber;
+    }
 
-        public class Products {
-            @Getter
-            @Setter
-            private CertificateType certificateDocument;
-            @Getter
-            @Setter
-            private String certificateDocumentDate;
-            @Getter
-            @Setter
-            private String certificateDocumentNumber;
-            @Getter
-            @Setter
-            private String productionDate;
-            @Getter
-            @Setter
-            private String tnvedCode;
-            @Getter
-            @Setter
-            private String uitCode;
-            @Getter
-            @Setter
-            private String uituCode;
+    @Getter
+    @Setter
+    public static class Description {
+        private String participantInn;
+    }
 
-            public enum CertificateType {
-                CONFORMITY_CERTIFICATE, CONFORMITY_DECLARATION
-            }
-        }
+    @Getter
+    @Setter
+    public static class Product {
+        private String certificateDocument;
+        private String certificateDocumentDate;
+        private String certificateDocumentNumber;
+        private String tnvedCode;
+        private String uitCode;
+        private String uituCode;
     }
 }
